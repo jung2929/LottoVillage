4e78a9be4f20d9e57b02a003ec5aa8f2ac69762a
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-12-05 03:10:34 +0900

    Added Main APIs

--- config/express.js
+++ config/express.js
@@ -58,6 +58,7 @@ module.exports = function () {
     // Web
     require(process.cwd() + '/app/web/routes/user.server.route')(app);
     require(process.cwd() + '/app/web/routes/product.server.route')(app);
+    require(process.cwd() + '/app/web/routes/main.server.route')(app);
 
     app.use(express.static(process.cwd() + '/public'));
     return app;
