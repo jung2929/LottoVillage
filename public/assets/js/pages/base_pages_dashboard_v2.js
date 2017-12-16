8c4eca81be666e0b0c72c49eb8484d3f934cc558
JerryJung-MacBook Pro <jwh1269@naver.com>
2017-12-16 15:07:14 +0900

    Changed IP Address - 2

--- public/assets/js/pages/base_pages_dashboard_v2.js
+++ public/assets/js/pages/base_pages_dashboard_v2.js
@@ -4,7 +4,8 @@
  *  Description: Custom JS code used in Dashboard v2 Page
  */
 
-var url = "http://localhost:3000/";
+//var url = "http://localhost:3000/";
+var url = "http://13.124.207.144/";
 //var url = "http://192.9.44.53:65004/";
 
 var weekDate = [];
